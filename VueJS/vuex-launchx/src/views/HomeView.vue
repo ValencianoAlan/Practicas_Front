963966b96cc785e544ee21a7c533b554ce0140c1
Alan Valenciano <launchx00914@innovaccion.mx>
2022-03-24 15:04:19 -0600

    .

---
+++ VueJS/vuex-launchx/src/views/HomeView.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Counter/>
+    <ContadorCuadrado/>
+    <Botones/>
+  </div>
+</template>
+
+<script>
+import Counter from "../components/Contador.vue";
+import ContadorCuadrado from "../components/Cuadrado.vue";
+import Botones from "../components/Botones.vue";
+
+export default {
+
+  name: 'HomeView',
+  
+  components:{
+    Counter,
+    ContadorCuadrado,
+    Botones,
+  }
+}
+</script>
+
+<style>
+div{
+  margin-bottom: 10px;
+}
+
+.counter{
+  font-size: 80px;
+}
+
+.buttons button{
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
